68e59f944f0cef105bef85808cc48982794d8c58
Agata <agat0538@stud.kea.dk>
2021-05-21 13:29:30 +0200

    form styling

--- src/Form.js
+++ src/Form.js
@@ -1,32 +1,28 @@
-
-
 export default function Form() {
-    return (
-        <form>
-            <div className="Field">
-                <label for="cardnumber">card number</label>
-                <input type="text" id="cardnumber" title="cardname"></input>
-            </div>
+  return (
+    <form>
+      <div className="Field">
+        <label for="cardnumber">card number</label>
+        <input type="text" id="cardnumber" title="cardname" placeholder="1234 1234 1234 1234"></input>
+      </div>
 
-            <div className="Field">
-                <label for="name">owner</label>
-                <input type="text" id="name" title="name"></input>
-            </div>
+      <div className="Field">
+        <label for="name">owner</label>
+        <input type="text" id="name" title="name" placeholder="John Doe"></input>
+      </div>
 
-            <div className="Field">
-                <label for="expdate">exp date</label>
-                <input type="text" id="expmonth" title="expdate"></input>
-                <input type="text" id="expyear" title="expdate"></input>
-            </div>
+      <div className="Field">
+        <label for="expdate">exp date</label>
+        <input type="text" id="expmonth" title="expdate" placeholder="MM"></input>
+        <input type="text" id="expyear" title="expdate" placeholder="YYYY"></input>
+      </div>
 
-            <div className="Field">
-                <label for="cvc">cvc</label>
-                <input type="text" id="name" title="cvc"></input>
-            </div>
+      <div className="Field">
+        <label for="cvc">cvc</label>
+        <input type="text" id="name" title="cvc" placeholder="123"></input>
+      </div>
 
-            <button className="Card_Pay">pay</button>
-        </form>
-
-    )
-
+      <button className="Card_Pay">pay</button>
+    </form>
+  );
 }
