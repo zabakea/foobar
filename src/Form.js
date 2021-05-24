5e54b1fa46ba3d182dc79bbd7d499bce06ef0d55
Agata <agat0538@stud.kea.dk>
2021-05-24 12:51:14 +0200

    forms

--- src/Form.js
+++ src/Form.js
@@ -3,7 +3,8 @@ export default function Form() {
     <form>
       <div className="Field">
         <label for="cardnumber">card number</label>
-        <input type="text" id="cardnumber" title="cardname" minlength="16" maxlength="16" placeholder="1234 1234 1234 1234"></input>
+        <input type="text" id="cardnumber" title="cardname" pattern="[0-9] {16}" placeholder="1234 1234 1234 1234"></input>
+        <span className="Error_Cardnumber">Card number should be 16 digits long.</span>
       </div>
 
       <div className="Field">
@@ -13,16 +14,21 @@ export default function Form() {
 
       <div className="Field">
         <label for="expdate">exp date</label>
-        <input type="text" id="expmonth" title="expdate" maxlength="2" placeholder="MM"></input>
-        <input type="text" id="expyear" title="expdate" maxlength="4" placeholder="YYYY"></input>
+        <input type="text" id="expmonth" title="expdate" pattern="[0-9] {2}" placeholder="MM"></input>
+        <span className="Error_Month">Month should be 2 digits long.</span>
+        <input type="text" id="expyear" title="expdate" pattern="[0-9] {4}" placeholder="YYYY"></input>
+        <span className="Error_Year">Year should be 4 digits long.</span>
       </div>
 
       <div className="Field">
         <label for="cvc">cvc</label>
-        <input type="text" id="name" title="cvc" minlength="3" maxlength="4" placeholder="123"></input>
+        <input type="text" id="cvc" title="cvc" pattern="[0-9] {3,4}" placeholder="123"></input>
+        <span className="Error_CVC">CVC should be 3 or 4 digits long.</span>
       </div>
 
-      <button className="Card_Pay">pay</button>
+      <button type="submit" formNoValidate className="Card_Pay">
+        pay
+      </button>
     </form>
   );
 }
