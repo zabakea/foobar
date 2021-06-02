a6912b3c10f255e435ce608f6a41ced9ef2aa274
Miko <kozak.mikolaj@gmail.com>
2021-06-02 23:01:44 +0200

    on the way

--- src/Order.js
+++ src/Order.js
@@ -1,13 +1,27 @@
-import PlaceOrder from "./PlaceOrder";
-// import Queue from "./Queue";
-
 const Order = ({ handlePosting, missing, filled, payments }) => {
   return (
+    // <div className="Order">
+    //   <div className={`Inner_Order ${payments.length > 0 ? "" : "flipped"}`}>
+    //     <div className="PlaceOrder">
+    //       <button className={`PlaceOrderButton ${missing.length > 0 || filled.length === 0 ? "disable" : ""}`} onClick={handlePosting}>
+    //         Place Order
+    //       </button>
+    //     </div>
+    //     <div className="Queue">
+    //       <p>
+    //         <span className="QueuePlace">X</span> in queue
+    //       </p>
+    //       <p>
+    //         ID: <span className="UserID">X</span>
+    //       </p>
+    //     </div>
+    //   </div>
+    // </div>
     <div className="Order">
-      <div className={`Inner_Order ${payments.length > 0 ? "" : "flipped"}`}>
-        <PlaceOrder handlePosting={handlePosting} missing={missing} filled={filled} />
-        {/* <Queue /> */}
-      </div>
+      <h1 className="orderNumber">128</h1>
+      <button className={`PlaceOrderButton ${missing.length > 0 || filled.length === 0 ? "disable" : ""} ${payments.length > 0 ? "" : "flipped"}`} onClick={handlePosting}>
+        Place Order
+      </button>
     </div>
   );
 };
