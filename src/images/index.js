e610d98791b0bfa5f2d9916f3ed21481d284bfe1
Agata <agat0538@stud.kea.dk>
2021-06-03 18:56:37 +0200

    plus minus icons

--- src/images/index.js
+++ src/images/index.js
@@ -8,7 +8,9 @@ import row26 from "./row26.png";
 import ruinedchildhood from "./ruinedchildhood.png";
 import sleighride from "./sleighride.png";
 import steampunk from "./steampunk.png";
+import minus_ai from "./minus_ai.svg";
+import plus_ai from "./plus_ai.svg";
 
-const Images = { elhefe, fairytaleale, githop, hollabacklager, hoppilyeverafter, mowintime, row26, ruinedchildhood, sleighride, steampunk };
+const Images = { elhefe, fairytaleale, githop, hollabacklager, hoppilyeverafter, mowintime, row26, ruinedchildhood, sleighride, steampunk, minus_ai, plus_ai };
 
 export default Images;
