c32c7b64c65a05f9e30a2273ea1deb596da7dc86
Miko <kozak.mikolaj@gmail.com>
2021-06-20 13:08:08 +0200

    tweaks

--- src/LiveChat.js
+++ src/LiveChat.js
@@ -1,14 +1,12 @@
+import { useState } from "react";
 const LiveChat = () => {
+  const [devMes, setDevMes]= useState(false)
   return (
     <div className="liveChat">
       <div className="messages">
         <div className="mes">
           <p>table 1: </p>
           <p>Yo</p>
-        </div>
-        <div className="mes">
-          <p>table 2: </p>
-          <p>,|,,</p>
         </div>
         <div className="mes">
           <p>table 5: </p>
@@ -18,10 +16,14 @@ const LiveChat = () => {
           <p>table 3: </p>
           <p>I dont sell drugs</p>
         </div>
+        {devMes ? <div className="mes">
+          <p>dev Miko: </p>
+          <p>this feature is not ready yet, its close ...</p>
+        </div> : null}
       </div>
       <div className="userInput">
         <input type="text"></input>
-        <button>send</button>
+        <button onClick={() =>setDevMes(true)}>send</button>
       </div>
     </div>
   );
